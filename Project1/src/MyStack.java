74fbe971551373ee2b6dc4953a9bd1ce3040984c
cdushana <cdushana@gmail.com>
2015-10-04 12:56:47 -0700

    initialized 2 files (MyStack & StackTest)

--- Project1/src/MyStack.java
+++ Project1/src/MyStack.java
@@ -1,4 +1,59 @@
+import java.util.*;
 
-public class MyStack {
+public class MyStack<T>
+{
+	private class Node
+	{
+		public T element;
+		public Node link;
+	}
 
-}
+	private Node top;
+
+	public MyStack()
+	{
+		top = null;
+	}
+
+	// - push: a void method with a T type parameter; the parameter object is added on the top of
+	// the stack (to the beginning of the linked list).
+	public void push(T addThis)
+	{
+		Node next = new Node();
+		next.element = addThis;
+		next.link = top; //isnt it null?
+		top = next;
+	}
+
+	// - pop: has no parameters and returns a T type value; the top element is removed from the
+	// stack (the first node of the linked list is deleted) and is returned by the method.
+	public T pop()
+	{
+		if(isEmpty())
+		{
+			throw new EmptyStackException("Invalid operation on an empty stack");
+		}
+		T topElement = top.element;
+		top = top.link;
+		return topElement;
+	}
+
+	// - peek: has no parameters and returns a T type value – the value of the top element (the first
+	// element of the list) – without removing it from the list.
+	// If the stack is empty, throws an EmptyStackException type exception
+	public T peek()
+	{
+		if(isEmpty())
+		{
+			throw new EmptyStackException("Invalid operation on an empty stack");
+		}
+		return top.element;
+	}
+
+	// - isEmpty: has no parameters and returns a boolean value – true if the stack is empty, and
+	// false otherwise.
+	public boolean isEmpty()
+	{
+		return (top == null);
+	}
+}	
---
+++ Project1/src/StackTest.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class StackTest
+{
+	MyStack stringStack = new MyStack();
+	Scanner scanner = new Scanner(System.in);
+
+	System.out.println("Choose one of the following operations:\n
+			- push/add (enter the letter a)
+			- pop/delete (enter the letter d)
+			- peek (enter the letter p)
+			- check if the list is empty (enter the letter e)
+			- Quit (enter the letter q)");
+
+	while(!scanner.hasNext("q"))
+	{ //SUBSTRING // String class: getChars() OR charAt()
+		String input = new String();
+		input = scanner.next();
+		switch(input)
+		{
+			case "a": System.out.println(push(input) + "pushed in");
+
+			case "d": System.out.println(pop() + "pushed out");
+
+			case "p": System.out.println(peek() + "on the top"); // input read as one line
+
+			case "e": if(isEmpty()){System.out.println("empty");}
+					  else{System.out.println("not empty");}
+
+			case "q": System.out.println("quitting");
+				
+			default: System.out.println("Invalid choice");
+		}
+
+		System.out.println("Goodbye!"); // 5. Farewell message
+	}
+
+	// 4. For as long as the user does not choose to quit, do the following:
+	// - Prompt the user to enter a menu choice (do NOT print the menu here; remember, the menu
+	// should be output ONLY ONCE, in step 3, before entering this loop).
+	// - Input user’s choice. Although you need to input only one letter (a, d, p, e, q), read in the
+	// whole line and use the first letter only; this will eliminate any future issues if the user
+	// accidentally entered additional symbols here.
+	// - Analyze user’s choice (use a switch statement with default case which will execute for
+	// invalid menu choices). Arrange the execution of the requested operation; if necessary (push
+	// operation), ask for an input value and input it from a separate line (DON’T enter the
+	// input value on the same line as the user’s choice). After the execution of each request,
+	// output one of the following messages respectively: “5 pushed in”, “5 popped out”, “5 on
+	// the top”, “empty”, “not empty”, “quitting” (the value 5 is used just for an example).
+	// Remember that your stack is defined to hold strings so you should not differentiate
+	// numeric values from other strings.
+
+	// 	Important:
+	// 1. If the operation throws an exception (pop and peek), don’t let the program crash or
+	// terminate, handle it by outputting a message “Invalid operation on an empty stack”.
+	// 2. The input value for the push method is read as one whole line.
+	// 3. If the user entered a wrong letter for a menu choice, output a message “Invalid choice”
+	// and finish the execution of that request (do NOT try to loop in asking to correct it). 
+	// 5. After the user chooses to quit, finish the program with a farewell message.
+
+}
