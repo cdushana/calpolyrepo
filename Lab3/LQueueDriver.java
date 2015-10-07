547f25163ac6e614c5f2dd870dc6ecc1bf3932bf
Devon Grove <devon@sports-it.com>
2015-10-06 21:03:56 -0700

    add some commenting for clarity

--- Lab3/LQueueDriver.java
+++ Lab3/LQueueDriver.java
@@ -17,9 +17,9 @@ public class LQueueDriver {
 
 		while(run)
 		{
+			// get only the first character of user input to scan
 			char result = ' ';
 			String input = scanner.nextLine();
-
 			if(input.length() > 0)
 			{
 				result = input.charAt(0);
@@ -31,13 +31,13 @@ public class LQueueDriver {
 					System.out.println("enqueue: ");
 					if(!scanner.hasNextInt())
 					{
-						System.out.println("Invalid value"); // Invalid choice is also outputted | FIX
-						scanner.nextLine();
+						System.out.println("Invalid value");
+						scanner.nextLine(); // this is done to make sure any extra newlines are thrown away
 					}
 					else
 					{
 						int toQueue = scanner.nextInt();
-						scanner.nextLine();
+						scanner.nextLine(); // this is done to make sure any extra newlines etc. are thrown away
 						queue.enqueue(toQueue);
 						System.out.println(toQueue + " enqueued");
 					}
