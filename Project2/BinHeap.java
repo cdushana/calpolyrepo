5624bc838e6408ceee69c1493859c94ec9db9fb1
Devon Grove <devon@sports-it.com>
2015-10-18 17:39:14 -0700

    lay out BinHeap.java

---
+++ Project2/BinHeap.java
@@ -0,0 +1,91 @@
+/* BinHeap is a Binary Heap implemented as an array.
+ *
+ * @authors Christiana Ushana & Devon Grove
+ * @version Project 2
+ * @version CPE103-9/10
+ * @version Fall 2015
+ * @version 10/18/15
+ */
+import java.util.*;
+
+public class BinHeap<T extends Comparable<? super T>> {
+	
+	public static class MyException extends RuntimeException {
+		
+		public MyException() {
+			super();
+		}
+		
+		public MyException(String message) {
+			super(message);
+		}
+	}
+	
+	private T[] collection;
+	private int size;
+	
+	/*
+	 * Constructs a BinHeap of size 100 if no parameter provided.
+	 */
+	public BinHeap() {
+		collection = (T[]) new Object[100]; // generic array declaration? | must use casting
+		size = 0;
+	}
+	
+	/*
+	 * Constructs a BinHeap of specified size provided.
+	 */
+	public BinHeap(int wantSize) {
+		collection = (T[]) new Object[wantSize]; // generic array declaration? | must use casting
+		size = 0;
+	}
+	
+	/*
+	 * Inserts the given element into the binary heap.
+	 * @param T - element to be inserted into array
+	 */
+	public void insert(T element) {
+		
+	}
+	
+	/*
+	 * Removes the binary heap root element (min) from the binary heap and returns it.
+	 * @return T - element to be inserted into array
+	 */
+	public T deleteMin() {
+		T root = collection[0];
+		// remove this element from collection
+		size--;
+		
+		return root;
+	}
+
+	/*
+	 * Identifies whether binary heap is empty or not.
+	 * @return boolean - empty status of binary heap
+	 */
+	public boolean isEmpty() {
+		boolean isEmpty = false;
+		
+		if(size == 0) {
+			isEmpty = true;
+		}
+		
+		return isEmpty;
+	}
+
+	/*
+	 * Getter for the size of the binary heap
+	 * @return int - number of elements in the binary heap
+	 */
+	public int size() {
+		return size;
+	}
+	
+	/*
+	 * @Override
+	 */
+	public String toString() {
+		return null;
+	}
+}
