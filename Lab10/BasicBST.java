7be767c10e0cfcb5a33aff5fefef06dfe9bab419
cdushana <cdushana@gmail.com>
2015-10-28 13:44:29 -0700

    started Lab10

---
+++ Lab10/BasicBST.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class BasicBST
+{
+	private class BSTNode
+	{
+		int element;			// element of collection
+		BSTNode left, right; 	// left and right children
+	}
+
+	private BSTNode root;
+
+	public BSTNode()
+	{
+		root = null;
+	}
+
+	public void insert(int value)
+	{
+
+	}
+
+	public int countOdds()
+	{
+
+	}
+
+	public int height()
+	{
+
+	}
+
+	public int countLeaves()
+	{
+
+	}
+
+	public int countOneChildParents()
+	{
+
+	}
+}
