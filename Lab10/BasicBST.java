0083493187e8f5c895aa8c401e8778e235ca926f
cdushana <cdushana@gmail.com>
2015-10-30 13:45:12 -0700

    change, change, change...

--- Lab10/BasicBST.java
+++ Lab10/BasicBST.java
@@ -17,26 +17,55 @@ public class BasicBST
 
 	public void insert(int value)
 	{
-
+		insert(value, root);
+	}
+	
+	protected void insert(int value, BSTNode root)
+	{
+		if(root == null)
+		{
+			root.element = value;
+		}
+		else if()
 	}
 
 	public int countOdds()
 	{
 
 	}
-
+	
+	protected int countOdds()
+	{
+		
+	}
+	
 	public int height()
 	{
 
+	}
+	
+	protected int height()
+	{
+		
 	}
 
 	public int countLeaves()
 	{
 
 	}
+	
+	protected int countLeaves()
+	{
+		
+	}
 
 	public int countOneChildParents()
 	{
 
+	}
+	
+	protected int countOneChildParents()
+	{
+		
 	}
 }
