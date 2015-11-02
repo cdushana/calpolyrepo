91d02c1ba062df6226266ed19dc3382c6c99548e
Devon Grove <devon@sports-it.com>
2015-11-02 13:36:56 -0800

    Update LList.java

--- Lab11/LList.java
+++ Lab11/LList.java
@@ -2,64 +2,70 @@ public class LList<T>
 {
 	private Node head;
 
-	private class Node
-	{
+	private class Node {
 		public T element;
 		public Node link;
 
-		public Node()
-		{
+		public Node() {
 			element = null;
 			link = null;
 		}
 
-		public Node(T element, Node link)
-		{
+		public Node(T element, Node link) {
 			this.element = element;
 			this.link = link;
 		}
 	}
 
-	private class Iter
-	{
+	private class Iter {
 		public Node cursor;
 
-		public Iter()
-		{
+		public Iter() {
 			cursor = head;
 		}
 
-		public boolean hasNext()
-		{
-
+		public boolean hasNext() {
+			return (cursor.link != null);
 		}
 
-		public T next()
-		{
-
+		public T next() {
+			// check javadoc for what we do here
+			if(!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			
+			cursor = cursor.link;
+			
+			return cursor;
 		}
 
-		// remove method not supported...
-		public void remove()
-		{
-
+		// this method is not supported
+		public void remove() {
+			throw new UnsupportedOperationException();
 		}
-
 	}
 
-	public LList()
-	{
+	public LList() {
 		head = null;
 	}
 
-	public void iterator()
-	{
-
+	public void iterator() {
+		new Iter();
 	}
 
-	public void add(T item) // add to end of list, should NOT be using iterator here
-	{
-
+	// add to end of list, should NOT be using iterator here 
+	public void add(T item) {
+		if(head.element == null) {
+			head = new Node(item, null);
+		}
+		
+		else {
+			current = head;
+			while(current.link != null) {
+				current = current.link;
+			}
+			
+			current.link = new Node(item, null);
+		}
 	}
-
 }
