ffa1ab4512f3e8d744f0f39591525360f81c140a
Devon Grove <djgrove@calpoly.edu>
2015-11-13 10:18:17 -0800

    More Project 3 work

--- Lab13/src/MyHashTable.java
+++ Lab13/src/MyHashTable.java
@@ -16,30 +16,48 @@ public class MyHashTable<T>
 		}
 	}
 
-	// returns int value - the hash value of the parameter - object for your table
-	// obtain hasCode of param object, take val and get modulus over array.length
+	/*
+	 * Obtain the hash code of the given element
+	 * @param T - the object to obtain hash code for
+	 * @return int - hash value of the parameter for the table
+	 */
 	private int hash(T item) {
 		return item.hashCode() % array.length;
 	}
 
+	/*
+	 * Inserts the given item into the appropriate hash value of the table
+	 * @param T - element desired to add
+	 */
 	public void insert(T item) {
 		array[hash(item)].addFirst(item);
 	}
 
+	/*
+	 * Attempts to delete the given item from the hash table if it exists
+	 * @param T - the element desired to delete
+	 */
 	public void delete(T item) {
 		array[hash(item)].remove(item);
 	}
 
+	/*
+	 * Attempts to find the given item in the hash table and returns true if found
+	 * @param T - the element to attempt to find
+	 * @return boolean - whether the item could be found or not
+	 */
 	public boolean find(T item) {
 		return array[hash(item)].contains(item);
 	}
 
+	/*
+	 * Checks if the hash table is empty
+	 * @boolean - whether the hash table is empty or not
+	 */
 	public boolean isEmpty()
 	{
-		for(int i = 0; i < array.length; i++)
-		{
-			if(array[i].size() > 0)
-			{
+		for(int i = 0; i < array.length; i++) {
+			if(array[i].size() > 0) {
 				return false;
 			}
 		}
@@ -47,15 +65,17 @@ public class MyHashTable<T>
 		return true;
 	}
 
-	public void print()
-	{
-		for(int i = 0; i < array.length; i++)
-		{
+	/*
+	 * Prints the hash table
+	 */
+	public void print() {
+		for(int i = 0; i < array.length; i++) {
 			System.out.print(i + ": ");
-			for(int j = 0; j < array[i].size(); j++)
-			{
+			
+			for(int j = 0; j < array[i].size(); j++) {
 				System.out.print(array[i].get(j) + " ");
 			}
+			
 			System.out.println();
 		}
 	}
@@ -75,7 +95,13 @@ public class MyHashTable<T>
 	 * @return int - the number of elements in the table
 	 */
 	public int size() {
-		return 0;
+		int elements = 0;
+		
+		for(int i = 0; i < array.length; i++) {
+			elements = elements + array[i].size();
+		}
+		
+		return elements;
 	}
 	
 	/*
@@ -88,24 +114,53 @@ public class MyHashTable<T>
 		
 		// creates preorder iterator
 		public Iter() {
+			
+			// find first non-empty linked list
+			for(int k = 0; i < array.length; i++) {		
+				if(!array[k].isEmpty()) {
+					i = k;
+					break;
+				}
+			}
+			
+			int j = 0;
 		}
 		
 		/*
 		 * uses iterator to determine if there is a next value, boolean output
+		 * @return boolean - whether iterator has a next value or not
 		 */
 		public boolean hasNext() {
+			if(i < array.length) {
+				return true;
+			}
+			
+			return false;		
 		}
 		
+		
 		/*
-		 *	uses iterator to output what the next value is, T output
+		 * uses iterator to output what the next value is, T output
+		 * @return T - the next element output by the iteraotr
 		 */
 		public T next() {
-			return null;
+			if(!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			
+			// get the current element iteraotr is on to return later
+			T element = array[i].get(j)
+			
+			return element;
 		}
 		
 		// not allowed
 		public void remove() {
 			throw new UnsupportedOperationException();
 		}
+	}
+	
+	public Iterator<T> iterator() {
+		return new Iter();
 	}
 }
